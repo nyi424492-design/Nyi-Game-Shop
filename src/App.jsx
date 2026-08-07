5b8cbd22b4d9d74171d756e82368910ada3c4c68
nyi424492-design <nyi424492@gmail.com>
2026-08-07 22:01:16 +0630

    Create src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./pages/Home";
+import BottomNav from "./components/BottomNav";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <Home />
+      <BottomNav />
+    </div>
+  );
+}
+
+export default App;
