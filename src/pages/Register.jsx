dec787d07f79e56df7fa2a4e25ac73679831a8f4
nyi424492-design <nyi424492@gmail.com>
2026-08-07 22:50:07 +0630

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,51 @@
+import { useState } from "react";
+import { supabase } from "../supabase";
+import { useNavigate } from "react-router-dom";
+
+
+function Register(){
+
+const [phone,setPhone] = useState("");
+const [password,setPassword] = useState("");
+
+const navigate = useNavigate();
+
+
 const register = async()=>{
 
-const {data,error} =
-await supabase.auth.signUp({
 
-email,
-password
+// Phone ရှိပြီးသား စစ်မယ်
 
-});
+const {data:existUser} = await supabase
+.from("users")
+.select("*")
+.eq("phone", phone)
+.single();
+
+
+if(existUser){
+
+alert("ဒီဖုန်းနံပါတ်နဲ့ Account ရှိပြီးသားပါ");
+
+return;
+
+}
+
+
+// User အသစ်သိမ်းမယ်
+
+const {error} = await supabase
+.from("users")
+.insert([
+
+{
+phone: phone,
+password: password,
+role: "user",
+wallet: 0
+}
+
+]);
 
 
 if(error){
@@ -17,6 +56,81 @@ alert(error.message);
 
 alert("Account Created");
 
+navigate("/login");
+
 }
 
+
 };
+
+
+return(
+
+<div className="p-5">
+
+<h1 className="text-3xl font-bold">
+NYI GAME SHOP Register
+</h1>
+
+
+<input
+
+className="bg-zinc-900 p-3 mt-5 w-full rounded"
+
+placeholder="Phone Number"
+
+onChange={(e)=>setPhone(e.target.value)}
+
+/>
+
+
+
+<input
+
+className="bg-zinc-900 p-3 mt-3 w-full rounded"
+
+placeholder="Password"
+
+type="password"
+
+onChange={(e)=>setPassword(e.target.value)}
+
+/>
+
+
+
+<button
+
+className="bg-yellow-500 text-black p-3 mt-5 w-full rounded"
+
+onClick={register}
+
+>
+
+Create Account
+
+</button>
+
+
+
+<button
+
+className="mt-3 text-yellow-400"
+
+onClick={()=>navigate("/login")}
+
+>
+
+Already have account? Login
+
+</button>
+
+
+</div>
+
+)
+
+}
+
+
+export default Register;
