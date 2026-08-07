6bed6b06cc1a5785dc1b9e43e5fbedaef51b789d
nyi424492-design <nyi424492@gmail.com>
2026-08-07 22:33:40 +0630

    Create History.jsx

---
+++ src/pages/History.jsx
@@ -0,0 +1,19 @@
+function History(){
+
+return(
+<div className="p-5">
+
+<h1 className="text-3xl font-bold">
+📜 Order မှတ်တမ်း
+</h1>
+
+<div className="bg-zinc-900 p-4 mt-5 rounded-xl">
+Order မရှိသေးပါ
+</div>
+
+</div>
+)
+
+}
+
+export default History;
