03ee5d43e4c82cc58bd5a8b37ff1ae221bf8ed54
nyi424492-design <nyi424492@gmail.com>
2026-08-07 22:31:07 +0630

    Update BottomNav.jsx

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,13 +1,29 @@
+import { Link } from "react-router-dom";
+
 function BottomNav(){
 
 return(
-<div className="fixed bottom-0 left-0 right-0 bg-zinc-900 p-3 flex justify-around">
+<div className="fixed bottom-0 left-0 right-0 bg-zinc-900 p-3 flex justify-around text-sm">
 
-<button>🏠 Home</button>
-<button>📦 အော်ဒါ</button>
-<button>💰 ငွေဖြည့်</button>
-<button>📜 မှတ်တမ်း</button>
-<button>👤 Profile</button>
+<Link to="/">
+🏠 Home
+</Link>
+
+<Link to="/order">
+📦 အော်ဒါ
+</Link>
+
+<Link to="/deposit">
+💰 ငွေဖြည့်
+</Link>
+
+<Link to="/history">
+📜 မှတ်တမ်း
+</Link>
+
+<Link to="/profile">
+👤 Profile
+</Link>
 
 </div>
 )
