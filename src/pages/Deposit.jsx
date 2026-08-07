de0a6efb6f26c9005a095a25435c496c350363d0
nyi424492-design <nyi424492@gmail.com>
2026-08-07 22:33:24 +0630

    Create Deposit.jsx

---
+++ src/pages/Deposit.jsx
@@ -0,0 +1,24 @@
+function Deposit(){
+
+return(
+<div className="p-5">
+
+<h1 className="text-3xl font-bold">
+💰 ငွေဖြည့်ရန်
+</h1>
+
+<div className="bg-zinc-900 p-4 mt-5 rounded-xl">
+<p>WavePay / KBZPay ဖြင့် ငွေဖြည့်နိုင်ပါသည်</p>
+
+<button className="bg-yellow-500 text-black p-3 mt-4 rounded">
+ပြေစာတင်မယ်
+</button>
+
+</div>
+
+</div>
+)
+
+}
+
+export default Deposit;
