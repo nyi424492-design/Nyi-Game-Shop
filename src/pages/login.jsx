48eb2569e7bdffa893bebff392bbfe57da6e17bc
nyi424492-design <nyi424492@gmail.com>
2026-08-07 22:49:31 +0630

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
+import { supabase } from "../supabase";
 import { useNavigate } from "react-router-dom";
 
 
 function Login(){
 
-const [email,setEmail] = useState("");
+const [phone,setPhone] = useState("");
 const [password,setPassword] = useState("");
 
 const navigate = useNavigate();
@@ -14,21 +13,23 @@ const navigate = useNavigate();
 
 const login = async()=>{
 
-try{
+const {data,error} = await supabase
+.from("users")
+.select("*")
+.eq("phone", phone)
+.eq("password", password)
+.single();
 
-await signInWithEmailAndPassword(
-auth,
-email,
-password
-);
+
+if(error){
+
+alert("ဖုန်းနံပါတ် သို့မဟုတ် Password မှားနေပါတယ်");
+
+}else{
 
 alert("Login Success");
 
 navigate("/");
-
-}catch(error){
-
-alert(error.message);
 
 }
 
@@ -45,33 +46,55 @@ NYI GAME SHOP Login
 
 
 <input
+
 className="bg-zinc-900 p-3 mt-5 w-full rounded"
-placeholder="Email"
-onChange={(e)=>setEmail(e.target.value)}
+
+placeholder="Phone Number"
+
+onChange={(e)=>setPhone(e.target.value)}
+
 />
+
 
 
 <input
+
 className="bg-zinc-900 p-3 mt-3 w-full rounded"
+
 placeholder="Password"
+
 type="password"
+
 onChange={(e)=>setPassword(e.target.value)}
+
 />
 
 
+
 <button
+
 className="bg-yellow-500 text-black p-3 mt-5 w-full rounded"
+
 onClick={login}
+
 >
+
 Login
+
 </button>
 
 
+
 <button
+
 className="mt-3 text-yellow-400"
+
 onClick={()=>navigate("/register")}
+
 >
+
 Create Account
+
 </button>
 
 
