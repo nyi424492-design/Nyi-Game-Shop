4ffc64e07b7da9b536a3a12fad0a5680f61b0e9d
nyi424492-design <nyi424492@gmail.com>
2026-08-07 22:34:15 +0630

    Create Profile.jsx

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,23 @@
+function Profile(){
+
+return(
+<div className="p-5">
+
+<h1 className="text-3xl font-bold">
+👤 Profile
+</h1>
+
+<div className="bg-zinc-900 p-4 mt-5 rounded-xl">
+
+<p>User Name</p>
+<p>Email</p>
+<p>Wallet: 0 Ks</p>
+
+</div>
+
+</div>
+)
+
+}
+
+export default Profile;
