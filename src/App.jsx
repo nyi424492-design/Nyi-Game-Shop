bffaff8bea176e17c900831fcd0415607d06db57
nyi424492-design <nyi424492@gmail.com>
2026-08-07 22:28:42 +0630

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Order from "./pages/Order";
+
 import BottomNav from "./components/BottomNav";
 
+
 function App() {
+
   return (
-    <div className="min-h-screen bg-black text-white">
-      <Home />
-      <BottomNav />
-    </div>
+    <BrowserRouter>
+
+      <div className="min-h-screen bg-black text-white">
+
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/register" element={<Register />} />
+
+          <Route path="/order" element={<Order />} />
+
+        </Routes>
+
+
+        <BottomNav />
+
+      </div>
+
+    </BrowserRouter>
   );
 }
+
 
 export default App;
