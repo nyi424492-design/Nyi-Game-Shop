f687cd3b0ebc60894374d9163b3eb6ba8b71adff
nyi424492-design <nyi424492@gmail.com>
2026-08-07 23:58:58 +0630

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { supabase } from "../supabase";
 import { useNavigate } from "react-router-dom";
 
+
 function Register(){
 
 const [phone,setPhone] = useState("");
@@ -11,6 +12,7 @@ const navigate = useNavigate();
 
 
 const register = async()=>{
+
 
 const {data:existUser} = await supabase
 .from("users")
@@ -46,8 +48,28 @@ alert(error.message);
 }else{
 
 alert("Account Created");
+
 navigate("/login");
 
 }
 
 };
+
+
+
+return(
+
+<div className="p-5">
+
+<h1 className="text-3xl font-bold">
+NYI GAME SHOP Register
+</h1>
+
+
+<input
+
+className="bg-zinc-900 p-3 mt-5 w-full rounded"
+
+placeholder="Phone Number"
+
+onChange={(e)=>setPhone(e.target.value)}
