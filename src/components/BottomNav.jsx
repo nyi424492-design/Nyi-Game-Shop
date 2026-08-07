76beca2233afe1cd54acf11a4a47571d472e4e7c
nyi424492-design <nyi424492@gmail.com>
2026-08-07 22:03:00 +0630

    Create src/components/BottomNav.jsx

---
+++ src/components/BottomNav.jsx
@@ -0,0 +1,17 @@
+function BottomNav(){
+
+return(
+<div className="fixed bottom-0 left-0 right-0 bg-zinc-900 p-3 flex justify-around">
+
+<button>🏠 Home</button>
+<button>📦 အော်ဒါ</button>
+<button>💰 ငွေဖြည့်</button>
+<button>📜 မှတ်တမ်း</button>
+<button>👤 Profile</button>
+
+</div>
+)
+
+}
+
+export default BottomNav;
