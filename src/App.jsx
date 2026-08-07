3960d0dcd8340fbd22ea1c0c521e3d1d81872ed5
nyi424492-design <nyi424492@gmail.com>
2026-08-07 22:32:19 +0630

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Order from "./pages/Order";
 
+import Deposit from "./pages/Deposit";
+import History from "./pages/History";
+import Profile from "./pages/Profile";
+
 import BottomNav from "./components/BottomNav";
 
 
@@ -13,7 +17,7 @@ function App() {
   return (
     <BrowserRouter>
 
-      <div className="min-h-screen bg-black text-white">
+      <div className="min-h-screen bg-black text-white pb-16">
 
         <Routes>
 
@@ -24,6 +28,12 @@ function App() {
           <Route path="/register" element={<Register />} />
 
           <Route path="/order" element={<Order />} />
+
+          <Route path="/deposit" element={<Deposit />} />
+
+          <Route path="/history" element={<History />} />
+
+          <Route path="/profile" element={<Profile />} />
 
         </Routes>
 
